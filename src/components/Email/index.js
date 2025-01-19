1530f25f919ef7162e45a81ac64df06c1c644857
starlightglim <195524079+starlightglim@users.noreply.github.com>
2025-01-18 23:25:45 -0500

    centered text

--- src/components/Email/index.js
+++ src/components/Email/index.js
@@ -1,10 +1,10 @@
 const EmailForm = () => {
     return (
-      <form className="flex flex-col gap-3 max-w-xs w-full mx-auto pointer-events-auto opacity-0 animate-fade-in-delay-2">
+      <form className="flex flex-col gap-3 max-w-xs w-full mx-auto pointer-events-auto opacity-0 animate-fade-in-delay-2 my-8">
         <input
           type="email"
           placeholder="Enter your email"
-          className="w-full px-5 py-4 bg-white/10 rounded-full text-sm tracking-wider border border-white/20 backdrop-blur-lg"
+          className="w-full px-5 py-4 bg-white/10 rounded-full text-sm tracking-wider border border-white/20 backdrop-blur-lg text-center placeholder:text-center"
           required
         />
         <button
